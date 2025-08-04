c121c781ad77c06c1b949981620c0a51bc85c6d7
Женя Балабанов <biomannumberone@gmail.com>
2025-08-04 23:00:38 +0300

    feature: created components Button Title Paragraph

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Button from './components/Button/Button';
+import Paragraph from './components/Paragraph/Paragraph';
+import Title from './components/Title/Title';
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	return (
+		<>
+			<Title text={'Поиск'}/>
+			<br />
+			<Title text={'Вход'}/>
+			<br />
+			<Button text={'Искать'}/>
+			<Button text={'Войти в профиль'}/>
+			<br />
+			<Paragraph text={'Введите название фильма, сериала или мультфильма для поиска и добавления в избранное.'}/>
+			<br />
+			<Paragraph text={'After the devastating events of Avengers: Infinity War, the universe is in ruins due to the efforts of the Mad Titan, Thanos. With the help of remaining allies, the Avengers must assemble once more in order to undo Thanos\' actions and restore order to the universe once and for all, no matter what consequences may be in store.'}/>
+		</>
+	);
 }
 
-export default App
+export default App;
