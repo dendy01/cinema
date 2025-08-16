7874c64a0d7238348cc6e61af205432247b0c43f
Женя Балабанов <biomannumberone@gmail.com>
2025-08-17 02:00:56 +0300

    fix: paths to images

--- src/model/Cards.js
+++ src/model/Cards.js
@@ -3,56 +3,56 @@ export const cards = [
 		id: crypto.randomUUID(),
 		title: 'Black Widow',
 		stars: 324,
-		image: '/public/images/black-widow-image.png',
+		image: '/images/black-widow-image.png',
 		inFavorites: false
 	},
 	{
 		id: crypto.randomUUID(),
 		title: 'Shang Chi',
 		stars: 124,
-		image: '/public/images/shang-chi-image.png',
+		image: '/images/shang-chi-image.png',
 		inFavorites: true
 	},
 	{
 		id: crypto.randomUUID(),
 		title: 'Loki',
 		stars: 235,
-		image: '/public/images/loki-image.png',
+		image: '/images/loki-image.png',
 		inFavorites: true
 	},
 	{
 		id: crypto.randomUUID(),
 		title: 'How I Met Your Mother',
 		stars: 123,
-		image: '/public/images/your-mother-image.png',
+		image: '/images/your-mother-image.png',
 		inFavorites: false
 	},
 	{
 		id: crypto.randomUUID(),
 		title: 'Money Heist',
 		stars: 8125,
-		image: '/public/images/money-heist-image.png',
+		image: '/images/money-heist-image.png',
 		inFavorites: true
 	},
 	{
 		id: crypto.randomUUID(),
 		title: 'Friends',
 		stars: 123,
-		image: '/public/images/friends-image.png',
+		image: '/images/friends-image.png',
 		inFavorites: false
 	},
 	{
 		id: crypto.randomUUID(),
 		title: 'The Big Bang Theory',
 		stars: 12,
-		image: '/public/images/big-bang-image.png',
+		image: '/images/big-bang-image.png',
 		inFavorites: false
 	},
 	{
 		id: crypto.randomUUID(),
 		title: 'Two And a Half Men',
 		stars: 456,
-		image: '/public/images/half-men-image.png',
+		image: '/images/half-men-image.png',
 		inFavorites: false
 	}
 ];
